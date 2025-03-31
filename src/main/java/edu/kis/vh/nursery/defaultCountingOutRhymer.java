f2a0725f06aa222ff25f59a10c8750a2e6ecb2f1
student <251112@edu.p.lodz.pl>
2025-03-31 13:34:54 +0200

    6-8. Wykonany issue.

--- src/main/java/edu/kis/vh/nursery/defaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/defaultCountingOutRhymer.java
@@ -2,33 +2,44 @@ package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
 
-    private int[] NUMBERS = new int[12];
+    public static final int EMPTY_INDICATOR = -1;
+    private static final int MAX_SIZE = 12;
+    private int[] NUMBERS = new int[MAX_SIZE];
+    private static final int TOTAL = 11;
 
-    public int total = -1;
+    private int total = EMPTY_INDICATOR;
 
     public void countIn(int in) {
         if (!isFull())
             NUMBERS[++total] = in;
     }
 
+    public boolean isEmpty() {
+        return total == EMPTY_INDICATOR;
+    }
+
+    public boolean isFull() {
+        return total == TOTAL;
+    }
+
         public boolean callCheck() {
             return total == -1;
         }
         
-            public boolean isFull() {
-                return total == 11;
-            }
-        
-                protected int peekaboo() {
-                    if (callCheck())
-                        return -1;
-                    return NUMBERS[total];
-                }
-            
-                    public int countOut() {
-                        if (callCheck())
-                            return -1;
-                        return NUMBERS[total--];
-                    }
 
+        protected int peekaboo() {
+            if (callCheck())
+                return EMPTY_INDICATOR;
+                return NUMBERS[total];
+        }
+
+        public int countOut() {
+            if (callCheck())
+                return -1;
+            return NUMBERS[total--];
+        }
+
+    private int getTotal() {
+        return total;
+    }
 }
